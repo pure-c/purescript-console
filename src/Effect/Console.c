070ce2e3e6cd09c6201683698d497d5f8350adc8
Felix Schlitter <felixschlitter@protonmail.com>
2018-09-28 09:11:37 +1200

    Update 'purs_any_get_string' usage

--- src/Effect/Console.c
+++ src/Effect/Console.c
@@ -2,21 +2,21 @@
 #include <purescript.h>
 
 PURS_FFI_FUNC_2(Effect_Console_log, x, _, {
-	fprintf(stdout, "%s\n", purs_any_get_string(x)->data);
+	fprintf(stdout, "%s\n", purs_any_get_string(x));
 	return NULL;
 })
 
 PURS_FFI_FUNC_2(Effect_Console_error, x, _, {
-	fprintf(stderr, "%s\n", purs_any_get_string(x)->data);
+	fprintf(stderr, "%s\n", purs_any_get_string(x));
 	return NULL;
 })
 
 PURS_FFI_FUNC_2(Effect_Console_warn, x, _, {
-	fprintf(stderr, "%s\n", purs_any_get_string(x)->data);
+	fprintf(stderr, "%s\n", purs_any_get_string(x));
 	return NULL;
 })
 
 PURS_FFI_FUNC_2(Effect_Console_info, x, _, {
-	fprintf(stderr, "%s\n", purs_any_get_string(x)->data);
+	fprintf(stderr, "%s\n", purs_any_get_string(x));
 	return NULL;
 })
