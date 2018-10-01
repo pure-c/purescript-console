5554f008ef238671b98bc43567a85b7a5119f90f
Felix Schlitter <felixschlitter@protonmail.com>
2018-10-02 10:18:21 +1300

    Update C FFI for experiment/blockless

--- src/Effect/Console.c
+++ src/Effect/Console.c
@@ -4,19 +4,19 @@
 PURS_FFI_FUNC_2(Effect_Console_log, x, _, {
 	fprintf(stdout, "%s\n", purs_any_get_string(x));
 	return NULL;
-})
+});
 
 PURS_FFI_FUNC_2(Effect_Console_error, x, _, {
 	fprintf(stderr, "%s\n", purs_any_get_string(x));
 	return NULL;
-})
+});
 
 PURS_FFI_FUNC_2(Effect_Console_warn, x, _, {
 	fprintf(stderr, "%s\n", purs_any_get_string(x));
 	return NULL;
-})
+});
 
 PURS_FFI_FUNC_2(Effect_Console_info, x, _, {
 	fprintf(stderr, "%s\n", purs_any_get_string(x));
 	return NULL;
-})
+});
