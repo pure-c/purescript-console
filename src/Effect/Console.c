0140e0b4a57c120d4013835b8062f60687260ad1
Felix Schlitter <felixschlitter@protonmail.com>
2019-11-12 09:31:27 +1300

    Update for pure-c/purec#60

--- src/Effect/Console.c
+++ src/Effect/Console.c
@@ -1,22 +1,30 @@
 #include <stdio.h>
 #include <purescript.h>
 
-PURS_FFI_FUNC_2(Effect_Console_log, x, _, {
-	fprintf(stdout, "%s\n", purs_any_get_string(x));
-	return NULL;
-});
+PURS_FFI_FUNC_2(Effect_Console_log, x, _) {
+	const purs_str_t* s = purs_any_force_string(x);
+	fprintf(stdout, "%s\n", s->data);
+	PURS_RC_RELEASE(s);
+	return purs_any_null;
+}
 
-PURS_FFI_FUNC_2(Effect_Console_error, x, _, {
-	fprintf(stderr, "%s\n", purs_any_get_string(x));
-	return NULL;
-});
+PURS_FFI_FUNC_2(Effect_Console_error, x, _) {
+	const purs_str_t* s = purs_any_force_string(x);
+	fprintf(stderr, "%s\n", s->data);
+	PURS_RC_RELEASE(s);
+	return purs_any_null;
+}
 
-PURS_FFI_FUNC_2(Effect_Console_warn, x, _, {
-	fprintf(stderr, "%s\n", purs_any_get_string(x));
-	return NULL;
-});
+PURS_FFI_FUNC_2(Effect_Console_warn, x, _) {
+	const purs_str_t* s = purs_any_force_string(x);
+	fprintf(stderr, "%s\n", s->data);
+	PURS_RC_RELEASE(s);
+	return purs_any_null;
+}
 
-PURS_FFI_FUNC_2(Effect_Console_info, x, _, {
-	fprintf(stderr, "%s\n", purs_any_get_string(x));
-	return NULL;
-});
+PURS_FFI_FUNC_2(Effect_Console_info, x, _) {
+	const purs_str_t* s = purs_any_force_string(x);
+	fprintf(stdout, "%s\n", s->data);
+	PURS_RC_RELEASE(s);
+	return purs_any_null;
+}
